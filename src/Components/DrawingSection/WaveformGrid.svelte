b53da1c6f59f86d98731cffe3c31b8c84d12d3d5
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2022-01-18 20:51:52 -0700

    feat: logarithmic (perceptual) lines on drawing

--- src/Components/DrawingSection/WaveformGrid.svelte
+++ src/Components/DrawingSection/WaveformGrid.svelte
@@ -2,17 +2,26 @@
     export let w: number;
     export let h: number;
     export let color: string = 'rgba(255, 255, 255)';
-    export let nVertical: number = 40;
-    export let nHorizontal: number = 40;
+    export let nVertical: number = 20;
+    export let nHorizontal: number = 30;
+
+    const createLogarithmicHorizontals = (nHorizontal: number, h: number) => {
+        const halfH = h / 2;
+        const nPerSide = Math.floor(nHorizontal / 2) + 1;
+        const logScaleLine = (i) => h / nPerSide * Math.log2(i);
+        const scalar = halfH / logScaleLine(nPerSide - 1);
+        const topHalf = Array(nPerSide).fill(0)
+            .map((_, i) => logScaleLine(i) * scalar);
+        const bottomHalf = topHalf.map((val) => -val + h);
+        return topHalf.concat(bottomHalf);
+    }
 
     $: verticals = Array(nVertical).fill(0).map((_, i) => w / nVertical * i);
-    $: console.log(verticals)
-    $: horizontals = Array(nHorizontal).fill(0).map((_, i) => w / nHorizontal * i)
+    $: horizontals = createLogarithmicHorizontals(nHorizontal, h || 0);
 </script>
 
 
 <!-- VERTICAL LINES -->
-
 <line x1={w / 2} y1={0} x2={w / 2} y2={h} stroke={color} stroke-width={2} opacity={0.4}/>
 {#each verticals as x}
     <line x1={x} y1={h} x2={x} y2={0} stroke={color} stroke-width={1} opacity={0.1}/>
